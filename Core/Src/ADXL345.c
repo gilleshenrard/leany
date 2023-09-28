9c6a2e9827060f09c36461a422c5686bf27a15fc
Gilles Henrard <gilles.hen@gmail.com>
2023-09-28 19:27:52 +0200

    make use of the measurement mode

--- Core/Src/ADXL345.c
+++ Core/Src/ADXL345.c
@@ -9,12 +9,15 @@
 
 #define ADXL_HIGH_RESERVED_REG	0x1C	///< Number of the last reserved register
 
-#define ADXL_TIMEOUT_MS	10		///< SPI direct transmission timeout span in milliseconds
+#define ADXL_TIMEOUT_MS	10			///< SPI direct transmission timeout span in milliseconds
 
-#define ADXL_WRITE		0x00	///< MSB configuration for write operations
-#define ADXL_READ		0x80	///< MSB configuration for read operations
-#define ADXL_SINGLE		0x00	///< Bit 6 configuration for single register operations
-#define ADXL_MULTIPLE	0x40	///< Bit 6 configuration for multiple register operations
+#define ADXL_WRITE		0x00		///< MSB configuration for write operations
+#define ADXL_READ		0x80		///< MSB configuration for read operations
+#define ADXL_SINGLE		0x00		///< Bit 6 configuration for single register operations
+#define ADXL_MULTIPLE	0x40		///< Bit 6 configuration for multiple register operations
+
+#define ADXL_STANDBY_MODE	0x00	///< Power control bit 3 configuration for standby mode
+#define ADXL_MEASURE_MODE	0x08	///< Power control bit 3 configuration for measurement mode
 
 #define ENABLE_SPI	HAL_GPIO_WritePin(ADXL_CS_GPIO_Port, ADXL_CS_Pin, GPIO_PIN_RESET);	///< Macro used to enable the SPI communication towards the accelerometer
 #define DISABLE_SPI	HAL_GPIO_WritePin(ADXL_CS_GPIO_Port, ADXL_CS_Pin, GPIO_PIN_SET);	///< Macro used to disable the SPI communication towards the accelerometer
@@ -26,9 +29,11 @@ SPI_HandleTypeDef* ADXL_spiHandle = NULL;	///< SPI handle used with the ADXL345
  *
  * @param[in] handle SPI handle used
  */
-void ADXL345initialise(const SPI_HandleTypeDef* handle){
+HAL_StatusTypeDef ADXL345initialise(const SPI_HandleTypeDef* handle){
 	ADXL_spiHandle = (SPI_HandleTypeDef*)handle;
-	DISABLE_SPI
+
+	//set the ADXL in the measurement mode
+	return (ADXL345writeRegister(POWER_CONTROL, ADXL_MEASURE_MODE));
 }
 
 /**
@@ -41,6 +46,10 @@ void ADXL345initialise(const SPI_HandleTypeDef* handle){
 HAL_StatusTypeDef ADXL345readRegister(adxl345Registers_e registerNumber, uint8_t* value){
 	HAL_StatusTypeDef result;
 	uint8_t instruction = ADXL_READ | ADXL_SINGLE | registerNumber;
+
+	//if handle not set, error
+	if(ADXL_spiHandle == NULL)
+		return (HAL_ERROR);
 
 	//if register number above known, error
 	if(registerNumber > ADXL_NB_REGISTERS)
@@ -55,6 +64,69 @@ HAL_StatusTypeDef ADXL345readRegister(adxl345Registers_e registerNumber, uint8_t
 	result = HAL_SPI_Transmit(ADXL_spiHandle, &instruction, 1, ADXL_TIMEOUT_MS);
 	if(result == HAL_OK)
 		result = HAL_SPI_Receive(ADXL_spiHandle, value, 1, ADXL_TIMEOUT_MS);
+	DISABLE_SPI
+
+	return (result);
+}
+
+/**
+ * @brief Write a single register on the ADXL345
+ *
+ * @param[in] registerNumber Register number
+ * @param[in] value Register value
+ * @return Return value of SPI transmissions
+ */
+HAL_StatusTypeDef ADXL345writeRegister(adxl345Registers_e registerNumber, uint8_t value){
+	HAL_StatusTypeDef result;
+	uint8_t instruction = ADXL_WRITE | ADXL_SINGLE | registerNumber;
+
+	//if handle not set, error
+	if(ADXL_spiHandle == NULL)
+		return (HAL_ERROR);
+
+	//if register number above known, error
+	if(registerNumber > ADXL_NB_REGISTERS)
+		return (HAL_ERROR);
+
+	//if register number between 0x01 and 0x1C included, error
+	if((uint8_t)(registerNumber - 1) < ADXL_HIGH_RESERVED_REG)
+		return (HAL_ERROR);
+
+	//transmit the read instruction and receive the reply
+	ENABLE_SPI
+	result = HAL_SPI_Transmit(ADXL_spiHandle, &instruction, 1, ADXL_TIMEOUT_MS);
+	if(result == HAL_OK)
+		result = HAL_SPI_Transmit(ADXL_spiHandle, &value, 1, ADXL_TIMEOUT_MS);
+	DISABLE_SPI
+
+	return (result);
+}
+
+/**
+ * @brief Read several registers on the ADXL345
+ *
+ * @param[in] firstRegister Number of the first register to read
+ * @param[out] value Registers value array
+ * @param[in] size Number of registers to read
+ * @return Return value of SPI transmissions
+ */
+HAL_StatusTypeDef ADXL345readRegisters(adxl345Registers_e firstRegister, uint8_t* value, uint8_t size){
+	HAL_StatusTypeDef result;
+	uint8_t instruction = ADXL_READ | ADXL_MULTIPLE | firstRegister;
+
+	//if handle not set, error
+	if(ADXL_spiHandle == NULL)
+		return (HAL_ERROR);
+
+	//if register numbers above known, error
+	if(firstRegister > ADXL_NB_REGISTERS)
+		return (HAL_ERROR);
+
+	//transmit the read instruction and receive the reply
+	ENABLE_SPI
+	result = HAL_SPI_Transmit(ADXL_spiHandle, &instruction, 1, ADXL_TIMEOUT_MS);
+	if(result == HAL_OK)
+		result = HAL_SPI_Receive(ADXL_spiHandle, value, size, ADXL_TIMEOUT_MS);
 	DISABLE_SPI
 
 	return (result);
