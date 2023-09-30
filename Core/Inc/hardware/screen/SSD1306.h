6d50951e519f254c358d21388142c76ff45ca4ff
Gilles Henrard <gilles.hen@gmail.com>
2023-09-30 13:04:30 +0200

    create base structure for the SSD1306 module

---
+++ Core/Inc/hardware/screen/SSD1306.h
@@ -0,0 +1,8 @@
+#ifndef INC_HARDWARE_SCREEN_SSD1306_H_
+#define INC_HARDWARE_SCREEN_SSD1306_H_
+#include <stdint.h>
+
+void SSD1306initialise();
+uint16_t SSD1306update();
+
+#endif /* INC_HARDWARE_SCREEN_SSD1306_H_ */
---
+++ Core/Src/hardware/screen/SSD1306.c
@@ -0,0 +1,9 @@
+#include "SSD1306.h"
+
+void SSD1306initialise(){
+
+}
+
+uint16_t SSD1306update(){
+	return (0);
+}
