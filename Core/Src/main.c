e5664756f994b58d274ec05343bf636bc363e28a
Gilles Henrard <gilles.hen@gmail.com>
2023-10-02 19:07:57 +0200

    add a CS pin for SPI2 + add comments

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -261,14 +261,14 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(ADXL_CS_GPIO_Port, ADXL_CS_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, ADXL_CS_Pin|SSD1306_CS_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : ADXL_CS_Pin */
-  GPIO_InitStruct.Pin = ADXL_CS_Pin;
+  /*Configure GPIO pins : ADXL_CS_Pin SSD1306_CS_Pin */
+  GPIO_InitStruct.Pin = ADXL_CS_Pin|SSD1306_CS_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(ADXL_CS_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pin : ADXL_INT1_Pin */
   GPIO_InitStruct.Pin = ADXL_INT1_Pin;
