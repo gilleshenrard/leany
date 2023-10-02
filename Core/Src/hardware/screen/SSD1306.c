3ff67ed06946fdeef9d4a50477c693a130770a0a
Gilles Henrard <gilles.hen@gmail.com>
2023-10-02 19:07:57 +0200

    add a CS pin for SPI2 + add comments

--- Core/Src/hardware/screen/SSD1306.c
+++ Core/Src/hardware/screen/SSD1306.c
@@ -1,3 +1,11 @@
+/**
+ * @file SSD1306.c
+ * @brief Implement the functioning of the SSD1306 OLED screen via SPI and DMA
+ * @author Gilles Henrard
+ * @date 02/10/2023
+ *
+ * @note Datasheet : https://cdn-shop.adafruit.com/datasheets/SSD1306.pdf
+ */
 #include "SSD1306.h"
 
 SPI_HandleTypeDef* SSD_SPIhandle = NULL;
