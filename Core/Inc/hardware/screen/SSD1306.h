75e8b3dacbb3e52203047bd4799412508d180fbb
Gilles Henrard <gilles.hen@gmail.com>
2023-10-02 20:02:29 +0200

    prepare commands implementation

--- Core/Inc/hardware/screen/SSD1306.h
+++ Core/Inc/hardware/screen/SSD1306.h
@@ -2,8 +2,10 @@
 #define INC_HARDWARE_SCREEN_SSD1306_H_
 #include <stdint.h>
 #include <stm32f1xx.h>
+#include "SSD1306_registers.h"
 
 void SSD1306initialise(SPI_HandleTypeDef* handle);
 uint16_t SSD1306update();
+uint16_t SSD1306WriteValue(SSD1306register_e regNumber, uint8_t value);
 
 #endif /* INC_HARDWARE_SCREEN_SSD1306_H_ */
--- Core/Src/hardware/screen/SSD1306.c
+++ Core/Src/hardware/screen/SSD1306.c
@@ -8,12 +8,35 @@
  */
 #include "SSD1306.h"
 
-SPI_HandleTypeDef* SSD_SPIhandle = NULL;
+SPI_HandleTypeDef* SSD_SPIhandle = NULL;	///< SPI handle used with the SSD1306
 
+/**
+ * @brief Initialise the SSD1306
+ *
+ * @param handle SPI handle used
+ */
 void SSD1306initialise(SPI_HandleTypeDef* handle){
 	SSD_SPIhandle = handle;
 }
 
+/**
+ * @brief Update the SSD1306
+ *
+ * @return Return code
+ */
 uint16_t SSD1306update(){
+	return (0);
+}
+
+/**
+ * @brief Write a byte value to a register
+ *
+ * @param regNumber Register number
+ * @param value Value to write
+ * @return Return code
+ */
+uint16_t SSD1306WriteValue(SSD1306register_e regNumber, uint8_t value){
+	UNUSED(regNumber);
+	UNUSED(value);
 	return (0);
 }
