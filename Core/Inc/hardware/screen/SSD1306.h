f20b8874a0f6cf269ac7d958757dba18d834c090
Gilles Henrard <gilles.hen@gmail.com>
2023-10-03 19:10:50 +0200

    implement data transmission to the screen

--- Core/Inc/hardware/screen/SSD1306.h
+++ Core/Inc/hardware/screen/SSD1306.h
@@ -7,5 +7,6 @@
 void SSD1306initialise(SPI_HandleTypeDef* handle);
 uint16_t SSD1306update();
 uint16_t SSD1306sendCommand(SSD1306register_e regNumber, const uint8_t parameters[], uint8_t nbParameters);
+uint16_t SSD1306sendData(const uint8_t values[], uint16_t size);
 
 #endif /* INC_HARDWARE_SCREEN_SSD1306_H_ */
