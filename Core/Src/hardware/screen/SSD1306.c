24d12182044687f0038d208c7f842c8270d0dab5
Gilles Henrard <gilles.hen@gmail.com>
2023-10-03 19:10:50 +0200

    implement data transmission to the screen

--- Core/Src/hardware/screen/SSD1306.c
+++ Core/Src/hardware/screen/SSD1306.c
@@ -12,6 +12,7 @@
 #define SSD1306_SUCCESS			0x00
 #define SSD1306_SPI_TIMEOUT_MS	10U
 #define SSD1306_MAX_PARAMETERS	6U
+#define SSD1306_MAX_DATA_SIZE	1024U	///< Maximum data size (128 * 64 bits / 8 bits per bytes)
 
 #define SSD1306_ENABLE_SPI HAL_GPIO_WritePin(SSD1306_CS_GPIO_Port, SSD1306_CS_Pin, GPIO_PIN_RESET);
 #define SSD1306_DISABLE_SPI HAL_GPIO_WritePin(SSD1306_CS_GPIO_Port, SSD1306_CS_Pin, GPIO_PIN_SET);
@@ -23,6 +24,7 @@ SPI_HandleTypeDef* SSD_SPIhandle = NULL;	///< SPI handle used with the SSD1306
 const uint8_t contrastInit = SSD_CONTRAST_HIGHEST;
 const uint8_t clockInit = SSD_CLOCK_FREQ_MID | SSD_CLOCK_DIVIDER_1;
 const uint8_t chargePumpInit = SSD_ENABLE_CHG_PUMP;
+uint8_t screenBuffer[SSD1306_MAX_DATA_SIZE] = {0};
 
 /**
  * @brief Initialise the SSD1306
@@ -49,7 +51,8 @@ void SSD1306initialise(SPI_HandleTypeDef* handle){
 	SSD1306sendCommand(CLOCK_DIVIDE_RATIO, &clockInit, 1);
 	SSD1306sendCommand(CHG_PUMP_REGULATOR, &chargePumpInit, 1);
 	SSD1306sendCommand(DISPLAY_ON, NULL, 0);
-	SSD1306sendCommand(DISPLAY_ALL_ON, NULL, 0);
+	screenBuffer[0] = screenBuffer[1] = SSD_CONTRAST_HIGHEST;
+	SSD1306sendData(screenBuffer, SSD1306_MAX_DATA_SIZE);
 }
 
 /**
@@ -86,6 +89,36 @@ uint16_t SSD1306sendCommand(SSD1306register_e regNumber, const uint8_t parameter
 	//if command send OK, send all parameters
 	if(parameters && nbParameters && (result == HAL_OK))
 		result = HAL_SPI_Transmit(SSD_SPIhandle, (uint8_t*)parameters, nbParameters, SSD1306_SPI_TIMEOUT_MS);
+
+	//disable SPI and return status
+	SSD1306_DISABLE_SPI
+	return (result != HAL_OK);
+}
+
+/**
+ * @brief Send data to the screen GDDRAM to be displayed
+ *
+ * @param values Data bytes to display
+ * @param size Number of bytes to display
+ * @return
+ */
+uint16_t SSD1306sendData(const uint8_t values[], uint16_t size){
+	HAL_StatusTypeDef result;
+
+	//if nothing to send, exit
+	if(!values || !size)
+		return(0);
+
+	//if more bytes than sectors in the GDDRAM, error
+	if(size > SSD1306_MAX_DATA_SIZE)
+		return(1);
+
+	//set command pin and enable SPI
+	SSD1306_SET_DATA
+	SSD1306_ENABLE_SPI
+
+	//transmit the buffer all at once
+	result = HAL_SPI_Transmit(SSD_SPIhandle, (uint8_t*)values, size, SSD1306_SPI_TIMEOUT_MS);
 
 	//disable SPI and return status
 	SSD1306_DISABLE_SPI
