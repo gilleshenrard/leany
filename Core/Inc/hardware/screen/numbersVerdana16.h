ded114cc785546664e3210eb01ab64d81ab81676
Gilles Henrard <gilles.hen@gmail.com>
2023-10-17 22:59:50 +0200

    start implementing Verdana 16 characters

---
+++ Core/Inc/hardware/screen/numbersVerdana16.h
@@ -0,0 +1,22 @@
+#include <stdint.h>
+
+/**
+ * @brief Enumeration of all the characters available in Verdana 16
+ */
+typedef enum{
+	INDEX_0,
+	INDEX_1,
+	INDEX_2,
+	INDEX_3,
+	INDEX_4,
+	INDEX_5,
+	INDEX_6,
+	INDEX_7,
+	INDEX_8,
+	INDEX_9,
+	INDEX_DOT,
+	INDEX_DASH,
+	NB_NUMBERS
+}numbers_e;
+
+extern const uint8_t numbers_Verdana16[];
