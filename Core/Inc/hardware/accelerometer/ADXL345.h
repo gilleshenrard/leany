5e9d74cfb75f4dc1d9b904f40d582ef72355ad6c
Gilles Henrard <gilles.hen@gmail.com>
2023-10-22 15:55:19 +0200

    implement angle calculations

--- Core/Inc/hardware/accelerometer/ADXL345.h
+++ Core/Inc/hardware/accelerometer/ADXL345.h
@@ -6,5 +6,7 @@ extern volatile uint8_t adxlINT1occurred;
 
 HAL_StatusTypeDef ADXL345initialise(const SPI_HandleTypeDef* handle);
 uint16_t ADXL345update();
+float ADXL345getXangleDegrees();
+float ADXL345getYangleDegrees();
 
 #endif /* INC_ADXL345_H_ */
