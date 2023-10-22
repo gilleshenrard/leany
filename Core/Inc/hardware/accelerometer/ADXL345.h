0923204d47e82855122db521fdd33d0672fac4af
Gilles Henrard <gilles.hen@gmail.com>
2023-10-22 16:11:18 +0200

    implement flag to know if new values are available

--- Core/Inc/hardware/accelerometer/ADXL345.h
+++ Core/Inc/hardware/accelerometer/ADXL345.h
@@ -6,6 +6,7 @@ extern volatile uint8_t adxlINT1occurred;
 
 HAL_StatusTypeDef ADXL345initialise(const SPI_HandleTypeDef* handle);
 uint16_t ADXL345update();
+uint8_t ADXL345hasNewMeasurements();
 float ADXL345getXangleDegrees();
 float ADXL345getYangleDegrees();
 
