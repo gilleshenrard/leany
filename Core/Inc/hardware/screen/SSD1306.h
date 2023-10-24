4918aec61a8e380c89123ca12fd94596d4c81e2d
Gilles Henrard <gilles.hen@gmail.com>
2023-10-24 22:30:35 +0200

    add comments

--- Core/Inc/hardware/screen/SSD1306.h
+++ Core/Inc/hardware/screen/SSD1306.h
@@ -4,8 +4,7 @@
 #include <stm32f1xx.h>
 #include "errors.h"
 
-void SSD1306initialise(SPI_HandleTypeDef* handle);
-
+errorCode_u SSD1306initialise(SPI_HandleTypeDef* handle);
 errorCode_u SSD1306_printAngle(float angle, uint8_t page, uint8_t column);
 
 #endif /* INC_HARDWARE_SCREEN_SSD1306_H_ */
