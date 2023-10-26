b616291b78270f52c94ff911f33dc217d06719a4
Gilles Henrard <gilles.hen@gmail.com>
2023-10-26 18:09:55 +0200

    make code more readable

--- Core/Inc/hardware/screen/SSD1306.h
+++ Core/Inc/hardware/screen/SSD1306.h
@@ -5,10 +5,10 @@
 #include "errors.h"
 
 //screen defaults
-#define SSD1306_LINE1_PAGE			0U		///< Page number of the first screen line
-#define SSD1306_LINE1_COLUMN		0U		///< Column number of the first screen line
-#define SSD1306_LINE2_PAGE			3U		///< Page number of the second screen line
-#define SSD1306_LINE2_COLUMN		0U		///< Column number of the second screen line
+#define SSD1306_LINE1_PAGE		0U		///< Page number of the first screen line
+#define SSD1306_LINE1_COLUMN	0U		///< Column number of the first screen line
+#define SSD1306_LINE2_PAGE		3U		///< Page number of the second screen line
+#define SSD1306_LINE2_COLUMN	0U		///< Column number of the second screen line
 
 errorCode_u SSD1306initialise(SPI_HandleTypeDef* handle);
 errorCode_u SSD1306_printAngle(float angle, uint8_t page, uint8_t column);
