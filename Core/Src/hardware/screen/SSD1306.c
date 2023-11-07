f4711bdbd18ed7fba2b3d8f0f9c5d6421563ead8
Gilles Henrard <gilles.hen@gmail.com>
2023-11-07 21:38:35 +0100

    rename static variables

--- Core/Src/hardware/screen/SSD1306.c
+++ Core/Src/hardware/screen/SSD1306.c
@@ -84,13 +84,13 @@ static const SSD1306init_t initCommands[SSD1306_NB_INIT_REGISERS] = {			///< Arr
 };
 
 //state variables
-static SPI_HandleTypeDef*	SSD_SPIhandle = NULL;						///< SPI handle used with the SSD1306
-static screenState			state = stIdle;								///< State machine current state
-static uint8_t				screenBuffer[SSD1306_MAX_DATA_SIZE] = {0};	///< Buffer used to send data to the screen
-volatile uint16_t			screenTimer_ms = 0;							///< Timer used with screen SPI transmissions
-static float				nextAngle;									///< Buffer used to store an angle to print
-static uint8_t				nextPage;									///< Buffer used to store a page at which to print
-static uint8_t				nextColumn;									///< Buffer used to store a column at which to print
+volatile uint16_t			screenTimer_ms = 0;						///< Timer used with screen SPI transmissions
+static SPI_HandleTypeDef*	_SSD_SPIhandle = NULL;					///< SPI handle used with the SSD1306
+static screenState			_state = stIdle;						///< State machine current state
+static uint8_t				_screenBuffer[SSD1306_MAX_DATA_SIZE];	///< Buffer used to send data to the screen
+static float				_nextAngle;								///< Buffer used to store an angle to print
+static uint8_t				_nextPage;								///< Buffer used to store a page at which to print
+static uint8_t				_nextColumn;							///< Buffer used to store a column at which to print
 
 
 /********************************************************************************************************************************************/
@@ -107,7 +107,7 @@ static uint8_t				nextColumn;									///< Buffer used to store a column at whic
  */
 errorCode_u SSD1306initialise(SPI_HandleTypeDef* handle){
 	errorCode_u result;
-	SSD_SPIhandle = handle;
+	_SSD_SPIhandle = handle;
 
 	//make sure to disable SSD1306 SPI communication
 	SSD1306_DISABLE_SPI
@@ -156,7 +156,7 @@ errorCode_u SSD1306sendCommand(SSD1306register_e regNumber, const uint8_t parame
 	SSD1306_ENABLE_SPI
 
 	//send the command byte
-	HALresult = HAL_SPI_Transmit(SSD_SPIhandle, &regNumber, 1, SSD1306_SPI_TIMEOUT_MS);
+	HALresult = HAL_SPI_Transmit(_SSD_SPIhandle, &regNumber, 1, SSD1306_SPI_TIMEOUT_MS);
 	if(HALresult != HAL_OK){
 		SSD1306_DISABLE_SPI
 		return (createErrorCodeLayer1(SEND_CMD, 2, HALresult, ERR_ERROR));
@@ -164,7 +164,7 @@ errorCode_u SSD1306sendCommand(SSD1306register_e regNumber, const uint8_t parame
 
 	//if command send OK, send all parameters
 	if(parameters && nbParameters){
-		HALresult = HAL_SPI_Transmit(SSD_SPIhandle, (uint8_t*)parameters, nbParameters, SSD1306_SPI_TIMEOUT_MS);
+		HALresult = HAL_SPI_Transmit(_SSD_SPIhandle, (uint8_t*)parameters, nbParameters, SSD1306_SPI_TIMEOUT_MS);
 		if(HALresult != HAL_OK)
 			result = createErrorCodeLayer1(SEND_CMD, 3, HALresult, ERR_ERROR); 		// @suppress("Avoid magic numbers")
 	}
@@ -200,7 +200,7 @@ errorCode_u SSD1306sendData(const uint8_t values[], uint16_t size){
 	SSD1306_ENABLE_SPI
 
 	//transmit the buffer all at once
-	HALresult = HAL_SPI_Transmit(SSD_SPIhandle, (uint8_t*)values, size, SSD1306_SPI_TIMEOUT_MS);
+	HALresult = HAL_SPI_Transmit(_SSD_SPIhandle, (uint8_t*)values, size, SSD1306_SPI_TIMEOUT_MS);
 	if(HALresult != HAL_OK)
 		result = createErrorCodeLayer1(SEND_DATA, 2, HALresult, ERR_ERROR);
 
@@ -234,7 +234,7 @@ errorCode_u SSD1306clearScreen(){
 		return (pushErrorCode(result, CLR_SCREEN, 2));
 
 	//send the buffer data
-	result = SSD1306sendData(screenBuffer, SSD1306_MAX_DATA_SIZE);
+	result = SSD1306sendData(_screenBuffer, SSD1306_MAX_DATA_SIZE);
 	if(IS_ERROR(result))
 		return (pushErrorCode(result, CLR_SCREEN, 3)); 		// @suppress("Avoid magic numbers")
 
@@ -247,7 +247,7 @@ errorCode_u SSD1306clearScreen(){
  * @return 1 if ready
  */
 uint8_t isScreenReady(){
-	return (state == stIdle);
+	return (_state == stIdle);
 }
 
 /**
@@ -266,12 +266,12 @@ errorCode_u SSD1306_printAngle(float angle, uint8_t page, uint8_t column){
 		return (createErrorCode(PRT_ANGLE, 1, ERR_WARNING));
 
 	//store the values
-	nextAngle = angle;
-	nextPage = page;
-	nextColumn = column;
+	_nextAngle = angle;
+	_nextPage = page;
+	_nextColumn = column;
 
 	//get to printing state
-	state = stPrintingAngle;
+	_state = stPrintingAngle;
 	return (ERR_SUCCESS);
 }
 
@@ -281,7 +281,7 @@ errorCode_u SSD1306_printAngle(float angle, uint8_t page, uint8_t column){
  * @return Return code of the current state
  */
 errorCode_u SSD1306update(){
-	return ((*state)());
+	return ((*_state)());
 }
 
 
@@ -308,28 +308,28 @@ errorCode_u stIdle(){
  */
 errorCode_u stPrintingAngle(){
 	uint8_t charIndexes[SSD1306_ANGLE_NB_CHARS] = {INDEX_PLUS, 0, 0, INDEX_DOT, 0, INDEX_DEG};
-	const uint8_t limitColumns[2] = {nextColumn, nextColumn + (VERDANA_CHAR_WIDTH * 6) - 1};
-	const uint8_t limitPages[2] = {nextPage, nextPage + 1};
-	uint8_t* iterator = screenBuffer;
+	const uint8_t limitColumns[2] = {_nextColumn, _nextColumn + (VERDANA_CHAR_WIDTH * 6) - 1};
+	const uint8_t limitPages[2] = {_nextPage, _nextPage + 1};
+	uint8_t* iterator = _screenBuffer;
 	errorCode_u result;
 	HAL_StatusTypeDef HALresult;
 
 	//if angle negative, replace plus sign with minus sign
-	if(nextAngle < SSD1306_NEG_THRESHOLD){
+	if(_nextAngle < SSD1306_NEG_THRESHOLD){
 		charIndexes[SSD1306_INDEX_SIGN] = INDEX_MINUS;
-		nextAngle = -nextAngle;
+		_nextAngle = -_nextAngle;
 	}
 
 	//fill the angle characters indexes array with the float values (tens, units, tenths)
-	charIndexes[SSD1306_INDEX_TENS] = (uint8_t)(nextAngle / SSD1306_FLOAT_FACTOR_10);
-	charIndexes[SSD1306_INDEX_UNITS] = ((uint8_t)nextAngle) % SSD1306_INT_FACTOR_10;
-	charIndexes[SSD1306_INDEX_TENTHS] = (uint8_t)((uint16_t)(nextAngle * SSD1306_FLOAT_FACTOR_10) % SSD1306_INT_FACTOR_10);
+	charIndexes[SSD1306_INDEX_TENS] = (uint8_t)(_nextAngle / SSD1306_FLOAT_FACTOR_10);
+	charIndexes[SSD1306_INDEX_UNITS] = ((uint8_t)_nextAngle) % SSD1306_INT_FACTOR_10;
+	charIndexes[SSD1306_INDEX_TENTHS] = (uint8_t)((uint16_t)(_nextAngle * SSD1306_FLOAT_FACTOR_10) % SSD1306_INT_FACTOR_10);
 
 	//fill the buffer with all the required bitmaps bytes (column by column, then character by character, then page by page)
-	for(nextPage = 0 ; nextPage < VERDANA_NB_PAGES ; nextPage++){
+	for(_nextPage = 0 ; _nextPage < VERDANA_NB_PAGES ; _nextPage++){
 		for(uint8_t character = 0 ; character < SSD1306_ANGLE_NB_CHARS ; character++){
-			for(nextColumn = 0 ; nextColumn < VERDANA_CHAR_WIDTH ; nextColumn++){
-				*iterator = verdana_16ptNumbers[charIndexes[character]][(VERDANA_CHAR_WIDTH * nextPage) + nextColumn];
+			for(_nextColumn = 0 ; _nextColumn < VERDANA_CHAR_WIDTH ; _nextColumn++){
+				*iterator = verdana_16ptNumbers[charIndexes[character]][(VERDANA_CHAR_WIDTH * _nextPage) + _nextColumn];
 				iterator++;
 			}
 		}
@@ -338,14 +338,14 @@ errorCode_u stPrintingAngle(){
 	//send the set start and end column addresses
 	result = SSD1306sendCommand(COLUMN_ADDRESS, limitColumns, 2);
 	if(IS_ERROR(result)){
-		state = stIdle;
+		_state = stIdle;
 		return (pushErrorCode(result, PRINTING_ANGLE, 1));
 	}
 
 	//send the set start and end page addresses
 	/*result = */SSD1306sendCommand(PAGE_ADDRESS, limitPages, 2);
 	if(IS_ERROR(result)){
-		state = stIdle;
+		_state = stIdle;
 		return (pushErrorCode(result, PRINTING_ANGLE, 2));
 	}
 
@@ -355,14 +355,14 @@ errorCode_u stPrintingAngle(){
 
 	//send the data
 	screenTimer_ms = SSD1306_SPI_TIMEOUT_MS;
-	HALresult = HAL_SPI_Transmit_DMA(SSD_SPIhandle, screenBuffer, SSD1306_ANGLE_NB_CHARS * VERDANA_NB_BYTES_CHAR);
+	HALresult = HAL_SPI_Transmit_DMA(_SSD_SPIhandle, _screenBuffer, SSD1306_ANGLE_NB_CHARS * VERDANA_NB_BYTES_CHAR);
 	if(HALresult != HAL_OK){
-		state = stIdle;
+		_state = stIdle;
 		return (createErrorCodeLayer1(PRINTING_ANGLE, 3, HALresult, ERR_ERROR)); 	// @suppress("Avoid magic numbers")
 	}
 
 	//get to next
-	state = stWaitingForTXdone;
+	_state = stWaitingForTXdone;
 	return (ERR_SUCCESS);
 }
 
@@ -376,17 +376,17 @@ errorCode_u stWaitingForTXdone(){
 	//if timer elapsed, stop DMA and error
 	if(!screenTimer_ms){
 		SSD1306_DISABLE_SPI
-		HAL_SPI_DMAStop(SSD_SPIhandle);
-		state = stIdle;
+		HAL_SPI_DMAStop(_SSD_SPIhandle);
+		_state = stIdle;
 		return (createErrorCode(WAITING_DMA_RDY, 1, ERR_ERROR));
 	}
 
 	//if TX not done yet, exit
-	if(HAL_SPI_GetState(SSD_SPIhandle) != HAL_SPI_STATE_READY)
+	if(HAL_SPI_GetState(_SSD_SPIhandle) != HAL_SPI_STATE_READY)
 		return (ERR_SUCCESS);
 
 	//disable SPI and get to idle state
 	SSD1306_DISABLE_SPI
-	state = stIdle;
+	_state = stIdle;
 	return (ERR_SUCCESS);
 }
