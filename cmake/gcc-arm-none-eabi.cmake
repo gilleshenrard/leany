bc0b849a84f4bf34f8ba524d75e7b88a65ee673f
Gilles Henrard <gilles.hen@gmail.com>
2024-01-04 00:37:33 +0100

    remove useless comments

--- cmake/gcc-arm-none-eabi.cmake
+++ cmake/gcc-arm-none-eabi.cmake
@@ -10,7 +10,7 @@ set (CMAKE_SYSTEM_NAME "Generic" CACHE STRING "")
 # Skip link step during toolchain validation.
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
-# Specify toolchain. NOTE When building from inside STM32CubeIDE the location of the toolchain is resolved by the "MCU Toolchain" project setting (via PATH).  
+# Specify toolchain.
 set(TOOLCHAIN_PREFIX   "arm-none-eabi-")
 set(CMAKE_C_COMPILER   "${TOOLCHAIN_PREFIX}gcc")
 set(CMAKE_ASM_COMPILER ${CMAKE_C_COMPILER})
