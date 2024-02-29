3fac7d214bad92145d4f9372188bdda239579462
Gilles Henrard <gilles.hen@gmail.com>
2024-02-29 23:05:23 +0100

    implement vertical axis icon

---
+++ Core/Src/hardware/screen/icons16.c
@@ -0,0 +1,26 @@
+#include "icons16.h"
+
+const uint8_t icons_16pt[NB_ICONS][ICONS_NB_CHARS] =
+{
+    // Vertical arrows (16 pixels wide)
+    //        #        
+    //       ###       
+    //      #####      
+    //     #######     
+    //       ###       
+    //       ###       
+    //       ###       
+    //       ###       
+    //       ###       
+    //       ###       
+    //       ###       
+    //       ###       
+    //     #######     
+    //      #####      
+    //       ###       
+    //        #        
+    [ARROWS_VERTICAL] = { 
+        0x00, 0x00, 0x00, 0x00, 0x08, 0x0C, 0xFE, 0xFF, 0xFE, 0x0C, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x10, 0x30, 0x7F, 0xFF, 0x7F, 0x30, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00,
+    },
+};
