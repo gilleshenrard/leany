463b9ed0462c5aa2c25d1b1f4d502ff73c3b2a67
Gilles Henrard <gilles.hen@gmail.com>
2024-03-01 13:46:23 +0100

    add comments

--- Core/Inc/hardware/screen/arrowsIcon.h
+++ Core/Inc/hardware/screen/arrowsIcon.h
@@ -2,8 +2,8 @@
 #define ARROWSICON_H_INCLUDED
 #include <stdint.h>
 
-#define ARROWSICON_NB_BYTES    (UINT8_MAX + 1)
-#define ARROWSICON_WIDTH       32U
+#define ARROWSICON_NB_BYTES    (UINT8_MAX + 1)  ///< Total number of bytes occupied by the arrows icon
+#define ARROWSICON_WIDTH       32U              ///< Pixel width of the icon
 
 extern const uint8_t arrowsIcon_32px[ARROWSICON_NB_BYTES];
 
