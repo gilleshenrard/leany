2c4375301c1afabfa9c3e1759cb25ca1b215dbd7
Gilles Henrard <gilles.hen@gmail.com>
2024-03-01 18:02:40 +0100

    keep only the dark trimmed icon

--- Core/Inc/hardware/screen/arrowsIcon.h
+++ Core/Inc/hardware/screen/arrowsIcon.h
@@ -5,8 +5,6 @@
 #define ARROWSICON_NB_BYTES    (UINT8_MAX + 1)  ///< Total number of bytes occupied by the arrows icon
 #define ARROWSICON_WIDTH       32U              ///< Pixel width of the icon
 
-extern const uint8_t arrowsIconLight_32px[ARROWSICON_NB_BYTES];
-extern const uint8_t arrowsIconDark_32px[ARROWSICON_NB_BYTES];
-extern const uint8_t arrowsIconDarkTrimmed_32px[ARROWSICON_NB_BYTES];
+extern const uint8_t arrowsIcon_32px[ARROWSICON_NB_BYTES];
 
 #endif
