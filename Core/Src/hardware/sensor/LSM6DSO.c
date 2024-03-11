fa19f4c458d9e60840f585d6cba9410e442bc227
Gilles Henrard <gilles.hen@gmail.com>
2024-03-11 18:19:04 +0100

    add comments

--- Core/Src/hardware/sensor/LSM6DSO.c
+++ Core/Src/hardware/sensor/LSM6DSO.c
@@ -5,6 +5,7 @@
  * @date 11/03/2024
  *
  * @note Additional information can be found in :
+ *   - Datasheet : https://www.st.com/resource/en/datasheet/lsm6dso.pdf
  *   - AN5192 (always-on 3-axis accelerometer and 3-axis gyroscope) : https://www.st.com/resource/en/application_note/an5192-lsm6dso-alwayson-3axis-accelerometer-and-3axis-gyroscope-stmicroelectronics.pdf
  *   - AN5226 (Finite State Machine) : https://www.st.com/resource/en/application_note/an5226-lsm6dso-finite-state-machine-stmicroelectronics.pdf
  *   - DT0058 (Design tip) : https://www.st.com/resource/en/design_tip/dt0058-computing-tilt-measurement-and-tiltcompensated-ecompass-stmicroelectronics.pdf
