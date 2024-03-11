b59817a2448ecd9b3b3d528831582a7132e53c98
Gilles Henrard <gilles.hen@gmail.com>
2024-03-12 00:00:49 +0100

    start implementing a manufacturer ID checking

--- Core/Inc/hardware/sensor/LSM6DSO.h
+++ Core/Inc/hardware/sensor/LSM6DSO.h
@@ -3,6 +3,9 @@
 #include <main.h>
 #include "errorstack.h"
 
+extern volatile uint16_t lsm6dsoTimer_ms;
+extern volatile uint16_t lsm6dsoSPITimer_ms;
+
 errorCode_u LSM6DSOinitialise(const SPI_TypeDef* handle);
 errorCode_u LSM6DSOupdate();
 
