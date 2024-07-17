6eb8c78b7ebfd9101764e2a833d6a1cec4e1fc76
Gilles Henrard <gilles.henrard@hotmail.com>
2024-07-17 18:54:57 +0200

    mimic the ADXL function, with the LSM6DSO (for now)

--- Core/Inc/hardware/sensor/LSM6DSO.h
+++ Core/Inc/hardware/sensor/LSM6DSO.h
@@ -6,17 +6,19 @@
 /**
  * @brief Enumeration of the axis of which to get measurements
  */
-typedef enum{
+typedef enum {
     X_AXIS = 0,
     Y_AXIS,
     Z_AXIS,
     NB_AXIS
-}axis_e;
+} axis_e;
 
 extern volatile uint16_t lsm6dsoTimer_ms;
 extern volatile uint16_t lsm6dsoSPITimer_ms;
 
 errorCode_u LSM6DSOinitialise(const SPI_TypeDef* handle);
 errorCode_u LSM6DSOupdate();
+uint8_t     LSM6DSOhasChanged(axis_e axis);
+int16_t     getAngleDegreesTenths(axis_e axis);
 
 #endif
