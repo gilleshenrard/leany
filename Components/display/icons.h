60825ac82eca643fcf422fb3261c452a67f89000
Gilles Henrard <gilles.henrard@hotmail.com>
2024-09-19 16:24:58 +0200

    ST7735S: implement a message queue resize verdana characters implement characters printing

--- Components/display/icons.h
+++ Components/display/icons.h
@@ -1,16 +1,15 @@
 #ifndef INC_HARDWARE_SCREEN_ICONS_H_
 #define INC_HARDWARE_SCREEN_ICONS_H_
 #include <stdint.h>
-#include "ST7735_initialisation.h"
 
 enum {
-    VERDANA_NB_ROWS    = 49U,
-    VERDANA_NB_COLUMNS = 39U,
+    VERDANA_NB_ROWS    = 37U,
+    VERDANA_NB_COLUMNS = 30U,
 };
 
 typedef enum {
-    BRIGHT_GRAY   = 0xEF7DU,  ///< #EEEEEE
-    DARK_CHARCOAL = 0x31A6U,  ///< #333333
+    BRIGHT_GRAY_BIGENDIAN   = 0x7DEFU,  ///< #EEEEEE in RGB565 with MSB and LSB switched
+    DARK_CHARCOAL_BIGENDIAN = 0xA631U,  ///< #333333 in RGB565 with MSB and LSB switched
 } colours_e;
 
 typedef enum {
@@ -26,8 +25,7 @@ typedef enum {
     VERDANA_9,
     VERDANA_PLUS,
     VERDANA_MIN,
-    VERDANA_COMMA,
-    VERDANA_DEG,
+    VERDANA_DOT,
     NB_CHARACTERS,
 } verdanaCharacter_e;
 
@@ -36,6 +34,6 @@ typedef enum {
  */
 typedef uint16_t pixel_t;
 
-void uncompressIconLine(registerValue_t* buffer, verdanaCharacter_e character, uint8_t line);
+void uncompressCharacter(pixel_t buffer[], verdanaCharacter_e character);
 
 #endif
