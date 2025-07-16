c072fe77ee5f7c0646faae2299dfb1a6602e387c
Gilles Henrard <gilles.henrard@hotmail.com>
2025-07-16 18:22:58 +0200

    properly clamp norms and alignment move dt calculation and update to sensorfusion make resetMahonyFilter reset timestamps reset filter on norm error add time delta domain validation implement integral clamping

--- firmware/Hardware/sensor/sensorfusion.h
+++ firmware/Hardware/sensor/sensorfusion.h
@@ -1,10 +1,32 @@
 #ifndef HARDWARE_SENSOR_SENSORFUSION_H
 #define HARDWARE_SENSOR_SENSORFUSION_H
-#include "memsBMI270.h"
+#include <stdint.h>
 
 enum {
-    QUATER_ALIGNMENT = 16U,  ///< Memory alignment of the quaternion structure
+    QUATER_ALIGNMENT    = 16U,  ///< Memory alignment of the quaternion structure
+    CONTEXT_ALIGNMENT   = 64U,  ///< Memory alignment of the mahony context structure
+    TIMEDELTA_ALIGNMENT = 16U,  ///< Memory alignment of the time delta structure
 };
+
+/**
+ * Enumeration of the axis of which to get measurements
+ */
+typedef enum {
+    X_AXIS = 0,
+    Y_AXIS,
+    Z_AXIS,
+    NB_AXIS
+} axis_e;
+
+/**
+ * Structure defining time delta
+ */
+typedef struct {
+    uint32_t currentTick;        ///< Current sensor tick value
+    uint32_t previousTick;       ///< Tick value at the last update
+    uint32_t maxTick;            ///< Maximum sensor tick value
+    float    resolutionSeconds;  ///< Tick resolution in [s]
+} __attribute__((aligned(TIMEDELTA_ALIGNMENT))) timedelta_t;
 
 /**
  * Structure defining a quaternion
@@ -16,10 +38,26 @@ typedef struct {
     float q3;  ///< Value which multiplies the unit vector along the Z axis
 } __attribute__((aligned(QUATER_ALIGNMENT))) quaternion_t;
 
-void  resetMahonyFilter(quaternion_t* currentAttitude, float integratedErrors[NB_AXIS]);
-void  updateMahonyFilter(quaternion_t* currentAttitude, float integratedErrors[NB_AXIS], float accelerometer_G[NB_AXIS],
-                         const float gyroscope_radps[NB_AXIS], float dtPeriod_sec);
-float angleAlongAxis(const quaternion_t* currentAttitude, axis_e axis);
-float getAttitudeAngle(const quaternion_t* currentAttitude);
+/**
+ * Structure defining a mahony filter context
+ */
+typedef struct {
+    quaternion_t attitude;                 ///< Current attitude quaternion
+    timedelta_t  dt;                       ///< Time delta between updates
+    float        errorIntegrals[NB_AXIS];  ///< Array containing the integrated errors
+    float        KP;                       ///< PI filter proportional gain
+    float        KI;                       ///< PI filter integral gain
+    uint8_t      badAccelerationCount;     ///< Number of bad accelerometer norms since reset
+    uint8_t      badQuaternionCount;       ///< Number of bad quaternion norms since reset
+} __attribute__((aligned(CONTEXT_ALIGNMENT))) mahonycontext_t;
+
+static const float PROPORTIONAL_GAIN = 2.5F;  ///< Propotional gain (KP) of the Mahony filter
+static const float INTEGRAL_GAIN     = 0.5F;  ///< Integral gain (KI) of the Mahony filter
+
+void  resetMahonyFilter(mahonycontext_t* context);
+void  updateMahonyFilter(mahonycontext_t* context, const float accelerometer_G[NB_AXIS],
+                         const float gyroscope_radps[NB_AXIS]);
+float angleAlongAxis(const mahonycontext_t* context, axis_e axis);
+float getAttitudeAngle(const mahonycontext_t* context);
 
 #endif
