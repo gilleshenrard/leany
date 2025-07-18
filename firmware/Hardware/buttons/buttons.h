96244c424657ad663823686740abb98bda1bba94
Gilles Henrard <gilleshenrard@users.noreply.github.com>
2025-07-18 10:40:40 +0200

    feat #35 Use and enfore REUSE copyright tool

--- firmware/Hardware/buttons/buttons.h
+++ firmware/Hardware/buttons/buttons.h
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Gilles Henrard <contact@gilleshenrard.com>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
 #ifndef INC_BUTTONS_H_
 #define INC_BUTTONS_H_
 #include <stdint.h>
