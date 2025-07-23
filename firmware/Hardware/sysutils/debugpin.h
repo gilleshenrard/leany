580ea168fc90cc96e40253476fe9b42d24cfb33d
Gilles Henrard <gilles.henrard@hotmail.com>
2025-07-23 14:51:25 +0200

    Implement and enforce Google-style formatting. fix #35

--- firmware/Hardware/sysutils/debugpin.h
+++ firmware/Hardware/sysutils/debugpin.h
@@ -6,14 +6,10 @@
 
 #ifndef HARDWARE_SYSUTILS_DEBUGPIN
 #define HARDWARE_SYSUTILS_DEBUGPIN
-#include "main.h"
+#include <main.h>
 
-static inline void setDebugPin(void) {
-    LL_GPIO_SetOutputPin(DEBUG_OUT_GPIO_Port, DEBUG_OUT_Pin);
-}
+static inline void setDebugPin(void) { LL_GPIO_SetOutputPin(DEBUG_OUT_GPIO_Port, DEBUG_OUT_Pin); }
 
-static inline void resetDebugPin(void) {
-    LL_GPIO_ResetOutputPin(DEBUG_OUT_GPIO_Port, DEBUG_OUT_Pin);
-}
+static inline void resetDebugPin(void) { LL_GPIO_ResetOutputPin(DEBUG_OUT_GPIO_Port, DEBUG_OUT_Pin); }
 
 #endif
