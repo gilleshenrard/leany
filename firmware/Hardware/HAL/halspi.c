a0e1a45313583afda07d8c3cdbfb7b81a63a74cf
Gilles Henrard <gilles.henrard@hotmail.com>
2025-07-23 14:51:25 +0200

    feat #35 - Implement and enforce Google-style formatting.

--- firmware/Hardware/HAL/halspi.c
+++ firmware/Hardware/HAL/halspi.c
@@ -8,41 +8,43 @@
  * @file halspi.c
  * @brief Implement a generic HAL for SPI communication
  * @author Gilles Henrard
- * @date 13/06/2025
+ * @date 25/07/2025
  */
 #include "halspi.h"
+
+#include <main.h>
+#include <portmacro.h>
+#include <projdefs.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <stm32f1xx_hal.h>
+#include <stm32f1xx_ll_dma.h>
+#include <stm32f1xx_ll_gpio.h>
+#include <stm32f1xx_ll_spi.h>
+
 #include "errorstack.h"
-#include "main.h"
-#include "portmacro.h"
-#include "projdefs.h"
-#include "stm32f1xx_hal.h"
-#include "stm32f1xx_ll_dma.h"
-#include "stm32f1xx_ll_gpio.h"
-#include "stm32f1xx_ll_spi.h"
 
 /**
  * @brief SPI Data/command pin status enumeration
  */
 typedef enum {
-    COMMAND = 0,  ///< Command is to be sent
-    DATA,         ///< Data is to be sent
-} DCgpio_e;
+    kCommand = 0,  ///< Command is to be sent
+    kData,         ///< Data is to be sent
+} DCgpio;
 
 /**
  * Enumeration of the function IDs
  */
 typedef enum {
-    SPI_READREGISTERS      = 1U,  ///< Function ID for the readRegisters()
-    SPI_WRITEREGISTERSCONT = 2U,  ///< Function ID for the writeRegistersAndContinue()
-    SPI_DMASEND            = 3U,  ///< Function ID for the sendDMA()
-} function_e;
+    kSPIreadRegisters = 1U,           ///< Function ID for the readRegisters()
+    kSPIwriteRegistersContinue = 2U,  ///< Function ID for the writeRegistersAndContinue()
+    kSPIdmaSend = 3U,                 ///< Function ID for the sendDMA()
+} FunctionCode;
 
 //state variables
 
-static inline void           setDataCommandGPIO(const spi_t* descriptor, DCgpio_e function);
-static volatile TaskHandle_t latestTask = NULL;  ///< Handle used by the FreeRTOS task
+static inline void setDataCommandGPIO(const SPI* descriptor, DCgpio function);
+static volatile TaskHandle_t latest_task = NULL;  ///< Handle used by the FreeRTOS task
 
 /********************************************************************************************************************************************/
 /********************************************************************************************************************************************/
@@ -51,9 +53,9 @@ static volatile TaskHandle_t latestTask = NULL;  ///< Handle used by the FreeRTO
  * Handler called upon DMA transfer complete
  */
 void st7735sDMAinterruptHandler(void) {
-    BaseType_t hasWoken = 0;
-    vTaskNotifyGiveFromISR(latestTask, &hasWoken);
-    portYIELD_FROM_ISR(hasWoken);
+    BaseType_t has_woken = 0;
+    vTaskNotifyGiveFromISR(latest_task, &has_woken);
+    portYIELD_FROM_ISR(has_woken);
 }
 
 /**
@@ -62,15 +64,15 @@ void st7735sDMAinterruptHandler(void) {
  * @param descriptor SPI descriptor to use
  * @param function Value of the data/command pin
  */
-static inline void setDataCommandGPIO(const spi_t* descriptor, DCgpio_e function) {
-    if(!descriptor->dataCommandPort) {
+static inline void setDataCommandGPIO(const SPI* descriptor, DCgpio function) {
+    if (!descriptor->data_command_port) {
         return;
     }
 
-    if(function == COMMAND) {
-        LL_GPIO_ResetOutputPin(descriptor->dataCommandPort, descriptor->dataCommandPin);
+    if (function == kCommand) {
+        LL_GPIO_ResetOutputPin(descriptor->data_command_port, descriptor->data_command_pin);
     } else {
-        LL_GPIO_SetOutputPin(descriptor->dataCommandPort, descriptor->dataCommandPin);
+        LL_GPIO_SetOutputPin(descriptor->data_command_port, descriptor->data_command_pin);
     }
 }
 
@@ -79,22 +81,21 @@ static inline void setDataCommandGPIO(const spi_t* descriptor, DCgpio_e function
  *
  * @param descriptor SPI descriptor to use
  */
-void closeTransmission(const spi_t* descriptor) {
-    LL_GPIO_SetOutputPin(descriptor->CSport, descriptor->pin);
-}
+void closeTransmission(const SPI* descriptor) { LL_GPIO_SetOutputPin(descriptor->cs_port, descriptor->pin); }
 
 /**
  * Receive a byte from the SPI bus
  * 
  * @param descriptor SPI descriptor to use
- * @param byteToTransmit Byte to transmit either as a command or as a filler to keep the SPI clock running
- * @param txStartTick Tick at which the transmission started
+ * @param byte_to_transmit Byte to transmit either as a command or as a filler to keep the SPI clock running
+ * @param tx_start_tick Tick at which the transmission started
  * @return uint8_t Received byte
  */
-uint8_t receiveSPIbyte(spi_t* descriptor, uint8_t byteToTransmit, uint32_t txStartTick) {
+uint8_t receiveSPIbyte(SPI* descriptor, uint8_t byte_to_transmit, uint32_t tx_start_tick) {
     //send the byte to transmit and wait for the reception to be complete
-    LL_SPI_TransmitData8(descriptor->handle, descriptor->readMask | byteToTransmit);
-    while((!LL_SPI_IsActiveFlag_RXNE(descriptor->handle)) && !timeout(txStartTick, SPI_TIMEOUT_MS)) {};
+    LL_SPI_TransmitData8(descriptor->handle, descriptor->read_mask | byte_to_transmit);
+    while ((!LL_SPI_IsActiveFlag_RXNE(descriptor->handle)) && !timeout(tx_start_tick, kSPItimeoutMS)) {
+    };
 
     //read the received byte (clears the rx buffer) and return it
     return LL_SPI_ReceiveData8(descriptor->handle);
@@ -104,54 +105,55 @@ uint8_t receiveSPIbyte(spi_t* descriptor, uint8_t byteToTransmit, uint32_t txSta
  * Burst read registers via SPI
  *
  * @param descriptor SPI descriptor to use
- * @param firstRegister Number of the first register to read
+ * @param first_register Number of the first register to read
  * @param[out] value Registers value array
  * @param size Number of registers to read
  * @return   Success
  * @retval 1 SPI handle or value buffer NULL
  * @retval 2 Timeout
  */
-errorCode_u readRegisters(spi_t* descriptor, spiregister_t firstRegister, spiregister_t value[], size_t size) {
+ErrorCode readRegisters(SPI* descriptor, SPIregister first_register, SPIregister value[], size_t size) {
     const uint8_t SPI_RX_FILLER = 0xFFU;  ///< Value to send as a filler while receiving multiple bytes
 
     //if no bytes to read, success
-    if(!size) {
-        return ERR_SUCCESS;
+    if (!size) {
+        return kSuccessCode;
     }
 
     //make sure neither the handle nor the buffer are NULL
-    if(!descriptor->handle || !value) {
-        return (createErrorCode(SPI_READREGISTERS, 1, ERR_CRITICAL));
+    if (!descriptor->handle || !value) {
+        return (createErrorCode(kSPIreadRegisters, 1, kErrorCritical));
     }
 
     //set timeout timer and enable CS
-    uint32_t SPIstartTick = HAL_GetTick();
-    LL_GPIO_ResetOutputPin(descriptor->CSport, descriptor->pin);
+    uint32_t spi_start_tick = HAL_GetTick();
+    LL_GPIO_ResetOutputPin(descriptor->cs_port, descriptor->pin);
 
     //send the read request and a dummy byte to synchronize the SPI clock
-    (void)receiveSPIbyte(descriptor, firstRegister, SPIstartTick);
-    (void)receiveSPIbyte(descriptor, SPI_RX_FILLER, SPIstartTick);
+    (void)receiveSPIbyte(descriptor, first_register, spi_start_tick);
+    (void)receiveSPIbyte(descriptor, SPI_RX_FILLER, spi_start_tick);
 
     //receive the bytes to read
     do {
-        *value = receiveSPIbyte(descriptor, SPI_RX_FILLER, SPIstartTick);
+        *value = receiveSPIbyte(descriptor, SPI_RX_FILLER, spi_start_tick);
         value++;
         size--;
-    } while(size && !timeout(SPIstartTick, SPI_TIMEOUT_MS));
+    } while (size && !timeout(spi_start_tick, kSPItimeoutMS));
 
     //wait for transaction to be finished and clear Overrun flag
-    while(LL_SPI_IsActiveFlag_BSY(descriptor->handle) && !timeout(SPIstartTick, SPI_TIMEOUT_MS)) {};
+    while (LL_SPI_IsActiveFlag_BSY(descriptor->handle) && !timeout(spi_start_tick, kSPItimeoutMS)) {
+    };
     LL_SPI_ClearFlag_OVR(descriptor->handle);
 
     //disable CS
-    LL_GPIO_SetOutputPin(descriptor->CSport, descriptor->pin);
+    LL_GPIO_SetOutputPin(descriptor->cs_port, descriptor->pin);
 
     //if timeout, error
-    if(timeout(SPIstartTick, SPI_TIMEOUT_MS)) {
-        return (createErrorCode(SPI_READREGISTERS, 2, ERR_WARNING));
+    if (timeout(spi_start_tick, kSPItimeoutMS)) {
+        return (createErrorCode(kSPIreadRegisters, 2, kErrorWarning));
     }
 
-    return (ERR_SUCCESS);
+    return (kSuccessCode);
 }
 
 /**
@@ -159,7 +161,7 @@ errorCode_u readRegisters(spi_t* descriptor, spiregister_t firstRegister, spireg
  * @details This function is blocking
  *
  * @param descriptor SPI descriptor to use
- * @param registerNumber Register to which write the values
+ * @param register_number Register to which write the values
  * @param[in] parameters Parameters to send
  * @param size Number of parameter bytes to write
  * @return	 Success
@@ -168,38 +170,40 @@ errorCode_u readRegisters(spi_t* descriptor, spiregister_t firstRegister, spireg
  * @retval 3 Register number out of range
  * @retval 4 Timeout
  */
-errorCode_u writeRegistersAndContinue(spi_t* descriptor, spiregister_t registerNumber, const spiregister_t parameters[],
-                                      size_t size) {
+ErrorCode writeRegistersAndContinue(SPI* descriptor, SPIregister register_number, const SPIregister parameters[],
+                                    size_t size) {
     //if handle not specified, error
-    if(!descriptor->handle) {
-        return (createErrorCode(SPI_WRITEREGISTERSCONT, 1, ERR_WARNING));
+    if (!descriptor->handle) {
+        return (createErrorCode(kSPIwriteRegistersContinue, 1, kErrorWarning));
     }
 
     //if no parameters provided and size not zero, error
-    if(!parameters && size) {
-        return (createErrorCode(SPI_WRITEREGISTERSCONT, 2, ERR_WARNING));
+    if (!parameters && size) {
+        return (createErrorCode(kSPIwriteRegistersContinue, 2, kErrorWarning));
     }
 
     //if register number above known or within the reserved range, error
-    if(registerNumber > descriptor->highestRegisterNumber) {
-        return (createErrorCode(SPI_WRITEREGISTERSCONT, 3, ERR_WARNING));
+    if (register_number > descriptor->highest_register_number) {
+        return (createErrorCode(kSPIwriteRegistersContinue, 3, kErrorWarning));
     }
 
     //set timeout timer and enable CS
-    uint32_t SPIstartTick = HAL_GetTick();
-    setDataCommandGPIO(descriptor, COMMAND);
-    LL_GPIO_ResetOutputPin(descriptor->CSport, descriptor->pin);
+    uint32_t spi_start_tick = HAL_GetTick();
+    setDataCommandGPIO(descriptor, kCommand);
+    LL_GPIO_ResetOutputPin(descriptor->cs_port, descriptor->pin);
 
     //send the write instruction
     LL_SPI_TransmitData8(descriptor->handle,
-                         descriptor->writeMask | registerNumber);  // cppcheck-suppress badBitmaskCheck
-    while(!LL_SPI_IsActiveFlag_TXE(descriptor->handle) && !timeout(SPIstartTick, SPI_TIMEOUT_MS)) {};
+                         descriptor->write_mask | register_number);  // cppcheck-suppress badBitmaskCheck
+    while (!LL_SPI_IsActiveFlag_TXE(descriptor->handle) && !timeout(spi_start_tick, kSPItimeoutMS)) {
+    };
 
     //write the value data
-    setDataCommandGPIO(descriptor, DATA);
-    while(size && !timeout(SPIstartTick, SPI_TIMEOUT_MS)) {
-        while(!LL_SPI_IsActiveFlag_TXE(descriptor->handle) && !timeout(SPIstartTick, SPI_TIMEOUT_MS)) {};
-        if(LL_SPI_IsActiveFlag_TXE(descriptor->handle)) {
+    setDataCommandGPIO(descriptor, kData);
+    while (size && !timeout(spi_start_tick, kSPItimeoutMS)) {
+        while (!LL_SPI_IsActiveFlag_TXE(descriptor->handle) && !timeout(spi_start_tick, kSPItimeoutMS)) {
+        };
+        if (LL_SPI_IsActiveFlag_TXE(descriptor->handle)) {
             LL_SPI_TransmitData8(descriptor->handle, *parameters);
         }
         parameters++;
@@ -207,15 +211,16 @@ errorCode_u writeRegistersAndContinue(spi_t* descriptor, spiregister_t registerN
     }
 
     //wait for transaction to be finished and clear Overrun flag
-    while(LL_SPI_IsActiveFlag_BSY(descriptor->handle) && !timeout(SPIstartTick, SPI_TIMEOUT_MS)) {};
+    while (LL_SPI_IsActiveFlag_BSY(descriptor->handle) && !timeout(spi_start_tick, kSPItimeoutMS)) {
+    };
     LL_SPI_ClearFlag_OVR(descriptor->handle);
 
     //if timeout, error
-    if(timeout(SPIstartTick, SPI_TIMEOUT_MS)) {
-        return (createErrorCode(SPI_WRITEREGISTERSCONT, 4, ERR_WARNING));
+    if (timeout(spi_start_tick, kSPItimeoutMS)) {
+        return (createErrorCode(kSPIwriteRegistersContinue, 4, kErrorWarning));
     }
 
-    return (ERR_SUCCESS);
+    return (kSuccessCode);
 }
 
 /**
@@ -223,14 +228,13 @@ errorCode_u writeRegistersAndContinue(spi_t* descriptor, spiregister_t registerN
  * @details This function is blocking
  *
  * @param descriptor SPI descriptor to use
- * @param registerNumber Register to which write the values
+ * @param register_number Register to which write the values
  * @param[in] parameters Parameters to send
  * @param size Number of parameter bytes to write
  * @return Value returned by writeRegistersAndContinue()
  */
-errorCode_u writeRegisters(spi_t* descriptor, spiregister_t registerNumber, const spiregister_t parameters[],
-                           size_t size) {
-    errorCode_u result = writeRegistersAndContinue(descriptor, registerNumber, parameters, size);
+ErrorCode writeRegisters(SPI* descriptor, SPIregister register_number, const SPIregister parameters[], size_t size) {
+    ErrorCode result = writeRegistersAndContinue(descriptor, register_number, parameters, size);
     closeTransmission(descriptor);
     return result;
 }
@@ -240,51 +244,51 @@ errorCode_u writeRegisters(spi_t* descriptor, spiregister_t registerNumber, cons
  *
  * @param dma       DMA descriptor to use
  * @param buffer    Data to send
- * @param nbBytes   Number of bytes to send
- * @param maxBytes  Maximum number of bytes which can be send (e.g. buffer size)
+ * @param nb_bytes   Number of bytes to send
+ * @param max_bytes  Maximum number of bytes which can be send (e.g. buffer size)
  * @retval 0 Success
  * @retval 1 Number of bytes exceeds the maximum
  * @retval 2 Timeout while waiting for the DMA transmit complete interrupt
  * @retval 3 DMA error
  */
-errorCode_u sendDMA(dma_t* dma, uint8_t* buffer, size_t nbBytes, size_t maxBytes) {
-    errorCode_u result;
+ErrorCode sendDMA(DMA* dma, uint8_t* buffer, size_t nb_bytes, size_t max_bytes) {
+    ErrorCode result;
 
-    if(nbBytes > maxBytes) {
-        return createErrorCode(SPI_DMASEND, 1, ERR_CRITICAL);
+    if (nb_bytes > max_bytes) {
+        return createErrorCode(kSPIdmaSend, 1, kErrorCritical);
     }
 
-    if(!nbBytes) {
-        return ERR_SUCCESS;
+    if (!nb_bytes) {
+        return kSuccessCode;
     }
 
-    latestTask = dma->task;
+    latest_task = dma->task;
 
     //configure the DMA transaction
-    LL_DMA_DisableChannel(dma->dma, dma->Channel);
+    LL_DMA_DisableChannel(dma->dma, dma->channel);
     LL_DMA_ClearFlag_GI5(dma->dma);
-    LL_DMA_EnableIT_TC(dma->dma, dma->Channel);
-    LL_DMA_ConfigAddresses(dma->dma, dma->Channel, (uint32_t)buffer, LL_SPI_DMA_GetRegAddr(dma->spi.handle),
+    LL_DMA_EnableIT_TC(dma->dma, dma->channel);
+    LL_DMA_ConfigAddresses(dma->dma, dma->channel, (uint32_t)buffer, LL_SPI_DMA_GetRegAddr(dma->spi.handle),
                            LL_DMA_DIRECTION_MEMORY_TO_PERIPH);
-    LL_DMA_SetDataLength(dma->dma, dma->Channel, nbBytes);  //must be reset every time
-    LL_DMA_EnableChannel(dma->dma, dma->Channel);
+    LL_DMA_SetDataLength(dma->dma, dma->channel, nb_bytes);  //must be reset every time
+    LL_DMA_EnableChannel(dma->dma, dma->channel);
     LL_SPI_EnableDMAReq_TX(dma->spi.handle);
 
-    result = ERR_SUCCESS;
+    result = kSuccessCode;
 
     //wait for measurements to be ready
-    if(ulTaskNotifyTake(pdTRUE, pdMS_TO_TICKS(SPI_TIMEOUT_MS)) == pdFALSE) {
-        result = createErrorCode(SPI_DMASEND, 2, ERR_ERROR);
+    if (ulTaskNotifyTake(pdTRUE, pdMS_TO_TICKS(kSPItimeoutMS)) == pdFALSE) {
+        result = createErrorCode(kSPIdmaSend, 2, kErrorError);
         goto finaliseDMA;
     }
 
-    if(LL_DMA_IsActiveFlag_TE5(dma->dma)) {
-        result = createErrorCode(SPI_DMASEND, 3, ERR_ERROR);
+    if (LL_DMA_IsActiveFlag_TE5(dma->dma)) {
+        result = createErrorCode(kSPIdmaSend, 3, kErrorError);
         goto finaliseDMA;
     }
 
 finaliseDMA:
-    LL_DMA_DisableChannel(dma->dma, dma->Channel);
+    LL_DMA_DisableChannel(dma->dma, dma->channel);
     closeTransmission(&dma->spi);
     return result;
 }
