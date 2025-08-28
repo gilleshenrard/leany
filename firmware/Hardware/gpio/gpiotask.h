92f02493ce0248b5c014ee6f392739672c4c0bb4
Gilles Henrard <gilles.henrard@hotmail.com>
2025-08-29 00:22:44 +0200

    fix #13 - Implement GPIO task with LED state machine

---
+++ firmware/Hardware/gpio/gpiotask.h
@@ -0,0 +1,11 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Gilles Henrard <contact@gilleshenrard.com>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef HARDWARE_GPIO_GPIO_H
+#define HARDWARE_GPIO_GPIO_H
+
+void createGPIOtask(void);
+#endif
