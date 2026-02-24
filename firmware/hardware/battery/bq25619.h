68b48a46f258f504faa7f3363288dbeac2cc6c54
Gilles Henrard <contact@gilleshenrard.com>
2026-02-24 18:21:43 +0100

    refactor: make battery status reentrant (#12)

--- firmware/hardware/battery/bq25619.h
+++ firmware/hardware/battery/bq25619.h
@@ -7,13 +7,12 @@
  */
 #ifndef HARDWARE_BATTERY_BQ25619_H
 #define HARDWARE_BATTERY_BQ25619_H
-#include <stdint.h>
-
+#include "bq25619_registers.inc"
 #include "errorstack.h"
 
 ErrorCode testBQ25619identifier(void);
 ErrorCode resetBQ25619(void);
 ErrorCode configureBQ25619(void);
-ErrorCode updateBQ25619status(uint32_t interrupt_received);
+ErrorCode updateBQ25619status(ChargerStatus* new_status, ChargerStatus* changes);
 
 #endif
