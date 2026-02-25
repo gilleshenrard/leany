87e36a485dd0bff24d51258c1bb8c1dc966731de
Gilles Henrard <contact@gilleshenrard.com>
2026-02-25 22:49:49 +0100

    ﻿refactor: enable second ADC rank and DMA (#12)

--- firmware/Core/Src/dma.c
+++ firmware/Core/Src/dma.c
@@ -44,6 +44,9 @@ void MX_DMA_Init(void)
   LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_DMA1);
 
   /* DMA interrupt init */
+  /* DMA1_Channel1_IRQn interrupt configuration */
+  NVIC_SetPriority(DMA1_Channel1_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(),5, 0));
+  NVIC_EnableIRQ(DMA1_Channel1_IRQn);
   /* DMA1_Channel5_IRQn interrupt configuration */
   NVIC_SetPriority(DMA1_Channel5_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(),5, 0));
   NVIC_EnableIRQ(DMA1_Channel5_IRQn);
