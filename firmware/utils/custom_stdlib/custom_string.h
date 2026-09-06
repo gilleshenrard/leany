b58b308cd4975c5abafeed6e00ec51900ff0d499
Gilles Henrard <contact@gilleshenrard.com>
2026-09-07 01:00:29 +0200

    docs: fix the GCC attributes for formatting (#103)
    (cherry picked from commit d9c84ff304ed8a6767c87170049a5c04399f653b)

--- firmware/utils/custom_stdlib/custom_string.h
+++ firmware/utils/custom_stdlib/custom_string.h
@@ -12,8 +12,14 @@
 #include <stddef.h>
 #include <stdint.h>
 
+// clang-format off
+__attribute__((format(printf, 3, 4)))
 int32_t custom_snprintf(char* buffer, size_t size, const char* format, ...);
+
+__attribute__((format(printf, 3, 0)))
 int32_t custom_vsnprintf(char* buffer, size_t size, const char* format, va_list* args);
+// clang-format on
+
 float custom_strtof(const char string[]);
 uint32_t custom_strtoi(const char string[]);
 uint32_t custom_strtoi_hexa(const char string[]);
